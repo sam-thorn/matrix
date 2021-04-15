f4c2d4e815f4cdf57583f314a5e7dad0952d00e6
Lorena Costa <loraphaela@gmail.com>
2021-04-15 17:41:41 +1200

    set background image

--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
-const path = require('path')
-const express = require('express')
+const path = require("path");
+const express = require("express");
 
-const widgets = require('./routes/widgets')
+const widgets = require("./routes/widgets");
 
-const server = express()
-server.use(express.json())
-server.use(express.static(path.join(__dirname, 'public')))
+const server = express();
+server.use(express.json());
+server.use(express.static(path.join(__dirname, "public")));
 
-server.use('/api/v1/widgets', widgets)
+server.use("/api/v1/widgets", widgets);
 
-module.exports = server
+module.exports = server;

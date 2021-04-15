3daabdc8bbcf23869e26d8a6581caa60fe9dab01
Lorena Costa <loraphaela@gmail.com>
2021-04-16 11:41:43 +1200

    add posters to matrix page

--- client/components/Bustblocker.jsx
+++ client/components/Bustblocker.jsx
@@ -4,7 +4,7 @@ import Movie from "./Movie";
 function Bustblocker() {
   return (
     <div className='page-bustblocker'>
-      <h1>Bustblocker</h1>
+      <h1 className="title">Bustblocker</h1>
       {/* AllMovies => AllMovies.map(movie => <Movie movie={ } />) */}
     </div>
   );

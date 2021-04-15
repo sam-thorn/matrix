d63272ed48ccc7248a19a2f9c617624852694053
johannbesas <johannbesas@gmail.com>
2021-04-16 11:58:43 +1200

    update to api calls on components

--- client/components/Bustblocker.jsx
+++ client/components/Bustblocker.jsx
@@ -2,21 +2,21 @@ import React, { useState, useEffect } from 'react'
 import request from 'superagent'
 import Movie from './Movie'
 
-function Bustblocker () {
+function Bustblocker() {
   const [movies, setMovies] = useState([])
+
   useEffect(() => {
     request.get('https://api.themoviedb.org/3/list/7092691?api_key=21404fdcc8723007e32347fcedff9bb4')
       .then(response => setMovies(response.body.items))
   }, [])
+
   return (
     <div className='page-bustblocker'>
       <h1>Bustblocker</h1>
       {/* AllMovies => AllMovies.map(movie => <Movie movie={ } />) */}
-      {movies.map(movie => <Movie movieData={movie}/>)}
+      {movies.map(movie => <Movie movieData={movie} />)}
     </div>
   )
 }
-
-{/* <p>{movie.title} {movie.release_date} {movie.vote_average} {movie.vote_count}</p> */}
 
 export default Bustblocker
--- client/components/Matrix.jsx
+++ client/components/Matrix.jsx
@@ -1,22 +1,21 @@
 import React, { useState, useEffect } from 'react'
-// import { request } from '../../server/server'
-import Movie from './Movie'
 import request from 'superagent'
+import Movie from './Movie'
 
-function Matrix () {
+function Matrix() {
   const [movies, setMovies] = useState([])
-  useEffect(() => {
-    request.get('https://api.themoviedb.org/3/collection/2344?api_key=21404fdcc8723007e32347fcedff9bb4')
-      .then(response => setMovies(response.body))
-  }, [])
 
+  useEffect(() => {
+    request.get('https://api.themoviedb.org/3/list/7092693?api_key=21404fdcc8723007e32347fcedff9bb4')
+      .then(response => setMovies(response.body.items))
+  }, [])
+  
   return (
     <div className='page-matrix'>
       <h1>Welcome to the Matrix</h1>
-      {/* MatrixMovies => MatrixMovies.map(movie => <Movie movie={ } />){' '} */}
-      {console.log(movies)}
+      {movies.map(movie => <Movie movieData={movie} />)}
     </div>
-  )
+  );
 }
 
-export default Matrix
+export default Matrix;

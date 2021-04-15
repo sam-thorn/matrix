8e1dfe800db56752fe259fae08f32eac8c511224
johannbesas <johannbesas@gmail.com>
2021-04-15 16:38:06 +1200

    initial set up of react components

---
+++ client/components/Matrix.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Movie from './Movie'
+
+
+function Matrix() {
+    return <>
+        {/* MatrixMovies => MatrixMovies.map(movie => <Movie movie={ } />) */}    </>
+}
+
+export default Matrix

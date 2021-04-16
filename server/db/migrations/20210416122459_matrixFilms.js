962a2f249bcc68e2a90336cc27519a8614dc01d6
Sam Thorn <spthornnz@gmail.com>
2021-04-16 12:38:25 +1200

    Revises migrations and seeds

--- server/db/migrations/20210416122459_matrixFilms.js
+++ server/db/migrations/20210416122459_matrixFilms.js
@@ -1,13 +1,9 @@
 exports.up = (knex) => {
   return knex.schema.createTable('matrixFilms', (table) => {
     table.increments('id').primary()
-    table.string('name')
-    table.string('genre')
-    table.integer('release_year')
-    table.string('director')
-    table.string('runtime')
-    table.text('short_summary')
-    table.text('long_summary')
+    table.string('title')
+    table.integer('likes')
+    table.integer('dislikes')
   })
 }
 

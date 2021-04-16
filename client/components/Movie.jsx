2ff87c6e6a7bdd40015c24fddcb3562fe24d7b19
Lorena Costa <loraphaela@gmail.com>
2021-04-16 14:47:33 +1200

    more styling

--- client/components/Movie.jsx
+++ client/components/Movie.jsx
@@ -5,10 +5,10 @@ function Movie(props) {
     <div>
         <img className="poster" src={(`/images/${props.movieData.title}.jpg`)} />
         <div className="details">
-            <p>Title: {props.movieData.title}</p>
-            <p>Release Date: {props.movieData.release_date}</p>
-            <p>Vote Average: {props.movieData.vote_average}</p>
-            <p>Vote Count: {props.movieData.vote_count}</p>
+            <p> <span className="props">Title:</span> {props.movieData.title}</p>
+            <p><span className="props">Release Date:</span> {props.movieData.release_date}</p>
+            <p><span className="props">Vote Average:</span> {props.movieData.vote_average}</p>
+            <p><span className="props">Vote Count:</span> {props.movieData.vote_count}</p>
         </div>
     </div>
     )}
